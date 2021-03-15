969e4d37d9e8cfc57f37c5292a8a7a85d9336ed3
Edd Tsao <etphonehomies@gmail.com>
2021-03-15 02:49:57 -0700

    Add vimium configuration

--- setup.sh
+++ setup.sh
@@ -5,29 +5,50 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 function bakAndSymlink {
   TARGET=$2
   SOURCE=$1
+  printf "\nMaybe symlink $SOURCE into $TARGET\n"
 
-  if [ -f $SOURCE ] ; then
-    if [ -f $TARGET ] && [ ! -L $TARGET ] ; then
-      mv $TARGET ${TARGET}_bak
-      echo "$TARGET already exists. Backing up as ${TARGET}_bak..."
+  ##### Source type: file
+  if [ -f "$SOURCE" ] ; then
+    # If target exists & it's not a symlink
+    if [ -f "$TARGET" ] && [ ! -L "$TARGET" ] ; then
+      mv "$TARGET" "${TARGET}_bak"
+      echo "...$TARGET already exists. Backing up as ${TARGET}_bak"
     fi
-    if [ ! -f $TARGET ] ; then
-      ln -s $DIR/$SOURCE $TARGET
-      return 0
+    if [ ! -f "$TARGET" ] ; then
+      TEST=$(dirname "$TARGET")
+      if [ -d "$(dirname "$TARGET")" ] ; then
+        echo "...symlinking $DIR/$SOURCE to $TARGET"
+        ln -s "$DIR/$SOURCE" "$TARGET"
+        return 0
+      else
+        echo "...parent directory does not exist"
+      fi
+    elif [ -L "$TARGET" ] ; then
+      echo "...already symlinked"
     fi
   fi
 
-  if [ -d $SOURCE ] ; then
-    if [ -d $TARGET ] && [ ! -L $TARGET ] ; then
-      mv $TARGET ${TARGET}_bak
-      echo "$TARGET already exists. Backing up as ${TARGET}_bak..."
+  ##### Source type: directory
+  if [ -d "$SOURCE" ] ; then
+    # If target exists & it's not a symlink
+    if [ -d "$TARGET" ] && [ ! -L "$TARGET" ] ; then
+      mv "$TARGET" "${TARGET}_bak"
+      echo "...$TARGET already exists. Backing up as ${TARGET}_bak"
     fi
-    if [ ! -d $TARGET ] ; then
-      ln -s $DIR/$SOURCE $TARGET
-      return 0
+    if [ ! -d "$TARGET" ] ; then
+      if [ -d "$(dirname "$TARGET")" ] ; then
+        echo "...symlinking $DIR/$SOURCE to $TARGET"
+        ln -s "$DIR/$SOURCE" "$TARGET"
+        return 0
+      else
+        echo "...parent directory does not exist"
+      fi
+    elif [ -L "$TARGET" ] ; then
+      echo "...already symlinked"
     fi
   fi
 
+  printf "...did not symlink $SOURCE\n\n"
   return 1
 }
 
@@ -80,3 +101,10 @@ bakAndSymlink rofi ~/.config/rofi
 # alacritty
 bakAndSymlink alacritty.yml ~/.config/alacritty.yml
 bakAndSymlink alacritty ~/.config/alacritty
+
+# vimium (vivaldi)
+bakAndSymlink vimium/000003.log "$HOME/.config/vivaldi/Default/Local Extension Settings/dbepggeogbaibhgnhhndojpepiihcmeb/000003.log"
+find ~/.config/vivaldi -mindepth 1 -maxdepth 1 -regextype posix-extended -regex ".*Profile [0-9]+" -type d | while read -r FILE_PATH ; do
+  IFS="/" SECTIONS=($FILE_PATH)
+  bakAndSymlink vimium/000003.log "$HOME/.config/vivaldi/${SECTIONS[-1]}/Local Extension Settings/dbepggeogbaibhgnhhndojpepiihcmeb/000003.log"
+done
